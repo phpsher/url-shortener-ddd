77a6a4c60f68d40d6558a456a60911207d0a39e8
Almaz Bogdanov <al.bogdanov@internet.ru>
2025-04-05 22:13:35 +0500

    Добавил тесты

--- app/Models/Url.php
+++ app/Models/Url.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Url extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'original_url',
         'alias'
---
+++ tests/Feature/UrlTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Url;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UrlTest extends TestCase
+{
+    public function testStoreUrlSuccessfully()
+    {
+        $response = $this->post('/api/v1/store', [
+            'original_url' => 'https://youtube.com',
+        ]);
+
+
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('urls', [
+            'original_url' => 'https://youtube.com',
+        ]);
+
+        $this->assertNotNull($response['short_url']);
+        $this->assertStringStartsWith(env('APP_URL'), $response['short_url']);
+    }
+
+    public function testShowUrlRedirectsToOriginalUrl()
+    {
+        $url = Url::factory()->create();
+
+        $response = $this->get('/' . $url->alias);
+
+        $response->assertRedirect($url->original_url);
+    }
+}
